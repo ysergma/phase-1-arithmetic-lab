f6740a6f7ba4936356007dd5122a6c7fe66f4c8c
Youssouf Sergma <sil3nthilldz@gmail.com>
2023-07-23 19:06:35 +0100

    completed assignement

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+var num1 = 2;
+var num2 = 31;
+let multiply = num1*num2;
+
+let random=Math.round(Math.random() * 10 + 1) ;
+
+console.log(Number.isInteger(random));
+
+console.log(random)
+var num3 = 64;
+var num4 = 10;
+let mod= num3 % num4;
+console.log(mod)
+
+var max = Math.max(4,8,15,20);
